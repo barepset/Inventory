33478ee57928491f3aaae707972f12d9a57daa4d
barepset <barepset@gmail.com>
2018-04-24 20:14:59 +0700

    Remove cek.php

--- cek.php
+++
@@ -1,9 +0,0 @@
-<div class="cek">
-    <form class="form-inline">
-  <div class="form-group">
-    <label for="code">Kode Barang</label>
-    <input type="text" class="form-control mx-sm-3" id="code">
-  </div>
-  <button type="submit" class="btn btn-primary">Cari</button>
-</form>
-</div>
